d79f9e9ca06865bccc8d680795c7b7225be1d285
Garchilla <dylan.king@ynov.com>
2025-09-25 10:15:23 +0200

    Adding base into git

---
+++ controller/controller.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, filename string, data map[string]string) {
+	tmpl := template.Must(template.ParseFiles("template/" + filename))
+	tmpl.Execute(w, data)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"Title":   "Acceuil",
+		"Message": "Bienvenue sur la page d'acceuil",
+	}
+	renderTemplate(w, "index.html", data)
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"Title":   "A propos",
+		"Message": "Ceci est la page à propos",
+	}
+	renderTemplate(w, "about.html", data)
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		msg := r.FormValue("msg")
+
+		data := map[string]string{
+			"Title":   "Contact",
+			"Message": "Merci " + name + " pour votre message : " + msg,
+		}
+		renderTemplate(w, "contact.html", data)
+		return
+	}
+
+	data := map[string]string{
+		"Title":   "Contact",
+		"Message": "Envoie-nous un message!",
+	}
+	renderTemplate(w, "contact.html", data)
+}
