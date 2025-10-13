89617ef979143d1731a97a2ec77c708e9c9621a2
Garchilla <dylan.king@ynov.com>
2025-10-13 12:24:12 +0200

    debut go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"power4/router"
+	"sync"
 )
 
 func main() {
@@ -12,3 +13,14 @@ func main() {
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	http.ListenAndServe(":8080", r)
 }
+
+const (
+	rows    = 6
+	columns = 7
+)
+
+var (
+	board  = make([][]string, rows)
+	player = "R"
+	mu     sync.Mutex
+)
