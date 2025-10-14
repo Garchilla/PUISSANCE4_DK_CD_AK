a6d82c11f47d515ccf312c29f305d01934c29bc6
Garchilla <dylan.king@ynov.com>
2025-10-14 09:54:03 +0200

    working button

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-	"power4/router"
 	"sync"
 )
-
-func main() {
-	r := router.New()
-
-	fmt.Println("Serveur démarré sur http://localhost:8080")
-	http.ListenAndServe(":8080", r)
-}
 
 const (
 	rows    = 6
@@ -24,3 +17,33 @@ var (
 	player = "R"
 	mu     sync.Mutex
 )
+
+func init() {
+	for i := range board {
+		board[i] = make([]string, columns)
+	}
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("about.html"))
+		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/action", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost && player == "R" {
+			fmt.Println("Faire apparaitre en rouge")
+			w.Write([]byte("Action effectuée côté serveur ! "))
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	fmt.Println("Serveur démarré sur http://localhost:8081/")
+	http.ListenAndServe(":8081", nil)
+}
